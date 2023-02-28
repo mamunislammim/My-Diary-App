8884d1c0ad80a46e34eeaaa7ebf4390b98c7d87a
mamunislammim <amarsopno678@gmail.com>
2023-02-28 23:06:38 +0600

    edit

--- lib/Screens/diary_page.dart
+++ lib/Screens/diary_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_diary/Models/diary_data_model.dart';
 import 'package:my_diary/Screens/write_diary_page.dart';
-  import 'edit_screen.dart';
+
+import 'edit_screen.dart';
 
 class DiaryPage extends StatefulWidget {
   const DiaryPage({Key? key}) : super(key: key);
@@ -16,6 +17,19 @@ class _DiaryPageState extends State<DiaryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: Image.asset("image/m.png",fit: BoxFit.fill,),
+            ),
+            Text("Developer : MD Mamun Islam",style: TextStyle(color: Colors.blue,fontSize: 20),),
+            Text("Contact : 01761810531",style: TextStyle(color: Colors.blue,fontSize: 17),)
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: const Text("My Diary"),
@@ -27,13 +41,13 @@ class _DiaryPageState extends State<DiaryPage> {
             ValueListenableBuilder(
                 valueListenable: Hive.box("openBox").listenable(),
                 builder: (_, box, widget) {
-                  List keys = box.keys.toList();
+                  List key = box.keys.toList();
 
                   return ListView.builder(
                       shrinkWrap: true,
-                      itemCount: keys.length,
+                      itemCount: key.length,
                       itemBuilder: (_, index) {
-                        DiaryData data = box.get(keys[index]);
+                        DiaryData data = box.get(key[index]);
 
                         return Padding(
                           padding:
@@ -43,8 +57,9 @@ class _DiaryPageState extends State<DiaryPage> {
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => TestData(data: data,
-
+                                      builder: (context) => EditScreen(
+                                            data: data,
+                                            index: key[index],
                                           )));
                             },
                             shape: RoundedRectangleBorder(
@@ -59,23 +74,24 @@ class _DiaryPageState extends State<DiaryPage> {
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            subtitle: data.comment!.length <= 20
-                                ? Text(
-                                    data.comment.toString(),
-                                    style: const TextStyle(
-                                      color: Colors.brown,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                : Text(
-                                    data.comment!.substring(0, 20).toString(),
-                                    style: const TextStyle(
-                                      color: Colors.brown,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
+                           // subtitle: Text("hive Index : ${key[index]} Local in : $index"),
+                            // subtitle: data.comment!.length <= 20
+                            //     ? Text(
+                            //        data.comment!.length.toString(),
+                            //         style: const TextStyle(
+                            //           color: Colors.brown,
+                            //           fontSize: 20,
+                            //           fontWeight: FontWeight.bold,
+                            //         ),
+                            //       )
+                            //     : Text(
+                            //         data.comment!.substring(0, 20).toString(),
+                            //         style: const TextStyle(
+                            //           color: Colors.brown,
+                            //           fontSize: 20,
+                            //           fontWeight: FontWeight.bold,
+                            //         ),
+                            //       ),
                             leading: const Icon(
                               Icons.note_alt_sharp,
                               color: Colors.pinkAccent,
@@ -84,18 +100,18 @@ class _DiaryPageState extends State<DiaryPage> {
                             trailing: Row(
                               mainAxisSize: MainAxisSize.min,
                               children: [
-                                InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => TestData(
-                                                   data: data,
-                                                  )));
-                                    },
-                                    child: const Icon(
-                                      Icons.edit,
-                                    )),
+                                // InkWell(
+                                //     onTap: () {
+                                //       Navigator.push(
+                                //           context,
+                                //           MaterialPageRoute(
+                                //               builder: (context) =>EditScreen (
+                                //                     data: data, index: keys[index],
+                                //                   )));
+                                //     },
+                                //     child: const Icon(
+                                //       Icons.edit,
+                                //     )),
                                 const SizedBox(
                                   width: 5,
                                 ),
@@ -110,7 +126,8 @@ class _DiaryPageState extends State<DiaryPage> {
                                           actions: [
                                             ElevatedButton(
                                                 onPressed: () {
-                                                  box.deleteAt(index);
+                                                  print("delete : ${key[index]}");
+                                                   box.delete(key[index]);
                                                   Navigator.pop(context);
                                                 },
                                                 child: const Text("Delete")),
--- lib/Screens/write_diary_page.dart
+++ lib/Screens/write_diary_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:my_diary/Models/diary_data_model.dart';
-import 'package:my_diary/Screens/diary_page.dart';
 
 class WriteDiaryPage extends StatefulWidget {
-   const WriteDiaryPage({Key? key,}) : super(key: key);
-
+  const WriteDiaryPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<WriteDiaryPage> createState() => _WriteDiaryPageState();
@@ -15,51 +15,88 @@ class _WriteDiaryPageState extends State<WriteDiaryPage> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _diaryController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Write Diary"),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _titleController,
-                decoration: InputDecoration(
-                    labelText: "Title",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white.withOpacity(.5)),
+                child: TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                      labelText: "Title",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: _diaryController,
-                minLines: 5,
-                maxLines: 50,
-                // expands: true,
-                decoration: InputDecoration(
-                    labelText: "Write here....",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white.withOpacity(.5)),
+                child: TextFormField(
+                  controller: _diaryController,
+                  minLines: 5,
+                  maxLines: 50,
+                  // expands: true,
+                  decoration: InputDecoration(
+                      labelText: "Write here....",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
               ),
             ),
             const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: (){
-                 setState(() {
+            // ElevatedButton(
+            //     onPressed: (){
+            //      setState(() {
+            //       Box box = Hive.box("openBox");
+            //        box.add(DiaryData(
+            //            title: _titleController.text,
+            //            comment: _diaryController.text));
+            //          Navigator.pop(context);
+            //      });
+            //       // Navigator.push(context, MaterialPageRoute(builder: (context)=> DiaryPage()));
+            //     },
+            //     child: const Text("Save To Diary"))
+
+            InkWell(
+              onTap: () {
+                setState(() {
                   Box box = Hive.box("openBox");
-                   box.add(DiaryData(
-                       title: _titleController.text,
-                       comment: _diaryController.text));
-                     Navigator.pop(context);
-                 });
-                  // Navigator.push(context, MaterialPageRoute(builder: (context)=> DiaryPage()));
-                },
-                child: const Text("Save To Diary"))
+                  box.add(DiaryData(
+                      title: _titleController.text,
+                      comment: _diaryController.text));
+                  Navigator.pop(context);
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.only(
+                    top: 13, bottom: 13, left: 20, right: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white),
+                child: const Text(
+                  "Save to Diary",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
           ],
         ),
       ),
